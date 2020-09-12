5b9aefa76da6ee409b88e9815663c9e8724e2cf1
Tristan Edwards <tristan.edwards@me.com>
2020-09-12 13:11:18 +0100

    Update Safello screenshot with redesign

--- src/components/Safello.svelte
+++ src/components/Safello.svelte
@@ -1,6 +1,23 @@
 <script>
+  import { onMount } from "svelte"
+
   import SafelloLogomark from "../icons/safello_logomark.svg"
   import Window from "./Window.svelte"
+
+  export let show = false
+
+  let windowsEl
+
+  onMount(() => {
+    function checkIfReach() {
+      const OFFSET = 200
+      show = window.innerHeight + window.scrollY > windowsEl.offsetTop + OFFSET
+    }
+
+    window.addEventListener("scroll", checkIfReach)
+
+    checkIfReach()
+  })
 </script>
 
 <div class="bg">
@@ -14,9 +31,9 @@
     a key player in the European Bitcoin industry.
   </p>
 
-  <div class="windows">
+  <div class="windows" bind:this={windowsEl} data-show={show}>
     <Window image="/images/safello_screenshots/buy.jpg" pos="left" />
-    <Window image="/images/safello_screenshots/landing.jpg" />
+    <Window image="/images/safello_screenshots/landing.png" />
     <Window image="/images/safello_screenshots/bitcoinse.jpg" pos="right" />
   </div>
 </div>
@@ -36,11 +53,32 @@
   }
 
   p a {
-    color: #56baa5;
+    color: #00c49d;
     font-weight: bold;
   }
 
   .windows {
     position: relative;
+  }
+
+  .windows :global(.window:not([data-pos="center"])) {
+    transition: transform 0.5s, opacity 0.3s;
+    opacity: 0;
+  }
+  .windows :global(.window[data-pos="left"]) {
+    transform: translateX(40%);
+  }
+  .windows :global(.window[data-pos="right"]) {
+    transform: translateX(-40%);
+  }
+
+  .windows[data-show="true"] :global(.window) {
+    opacity: 1;
+  }
+  .windows[data-show="true"] :global(.window[data-pos="left"]) {
+    transform: perspective(1000px) rotateY(-20deg) translateX(0%);
+  }
+  .windows[data-show="true"] :global(.window[data-pos="right"]) {
+    transform: perspective(1000px) rotateY(20deg) translateX(0%);
   }
 </style>
